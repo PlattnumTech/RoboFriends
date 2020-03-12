87729128ca10333282c586c5c62f0348c7a015e2
John Plattenburg jr <plattenb@usc.edu>
2020-03-12 01:15:06 -0500

    UPdate

--- src/App.js
+++ src/App.js
@@ -1,16 +1,42 @@
-import React from 'react';
+import React, {Component} from 'react';
 import CardList from "./CardList";
 import { robots } from "./robots";
 import SearchBox from './SearchBox';
 
-const App = () => {
-return (
-    <div>
-        <h1>RoboFriends</h1>
-        <SearchBox />
-        <CardList robots={robots}/>
-    </div>
-);
+
+
+
+class App extends Component {
+    constructor() {
+        super()
+        this.state = {
+            robots: robots,
+            searchfield: ''
+        }
+    }
+
+    onsearchChange = (event) => {
+       this.setState({searchfield: event.target.value}) //ALways use when looking to change state
+        const filteredRobots = this.state.robots.filter(robots =>{
+            return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
+        })
+}
+
+
+    render() {
+        const filteredRobots = this.state.robots.filter(robots =>{
+            return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
+        })
+        return (
+            <div className='tc'>
+                <h1>RoboFriends</h1>
+                <SearchBox searchChange={this.onsearchChange}/>
+                <CardList robots={filteredRobots} />
+            </div>
+        );
+
+    }
+
 
 }
 
